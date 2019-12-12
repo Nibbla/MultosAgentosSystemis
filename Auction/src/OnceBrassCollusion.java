1dd2abaea351c4ea1ed37d016faa0a9848008dc5
Nibbla <Niebisch.markus@gmx.de>
2019-12-12 04:39:32 +0100

    improvements, but getting messy. also problems if more sellers then buyers. and currently going off the script, as bids are done in parallel. in the homework description its expected that if somebody bids, he knows if he has won this round

--- Auction/src/OnceBrassCollusion.java
+++ Auction/src/OnceBrassCollusion.java
@@ -9,7 +9,7 @@ public class OnceBrassCollusion {
     public static Random rng;
 
     public static void main(String[] args){
-        OnceBrassCollusion.setRandomNumberGenerator(new Random(0));
+        OnceBrassCollusion.setRandomNumberGenerator(new Random(1));
         OnceBrassCollusion obc = new OnceBrassCollusion();
 
         obc.doAllExperiments();
@@ -17,9 +17,9 @@ public class OnceBrassCollusion {
 
     private void doAllExperiments() {
         int minK = 6;
-        int maxK = 12;
+        int maxK = 6;
         int minN = 6;
-        int maxN = 100;
+        int maxN = 6;
         int threasholdNIncrement = 12;
         double incrementalFactorN = 1.5;
         int minR = 4;
@@ -34,11 +34,11 @@ public class OnceBrassCollusion {
         for (int k = minK; k <= maxK; k++) {
             for (int n = minN; n <= maxN; n = increase(n, threasholdNIncrement, incrementalFactorN)) {
                 for (int r = minR; r < maxR; r = increase(r, 1, 1.5)) {
-                    BiddingStrategy[] bs = BiddingStrategy.getUniformSelection(k);
-                    BiddingStrategy[] bs2 = BiddingStrategy.getAllOfOneType(BiddingStrategy.Version1,k);
+                    BiddingStrategy[] bs1 = BiddingStrategy.getAllOfOneType(BiddingStrategy.Version1,k);
+                    BiddingStrategy[] bs2 = BiddingStrategy.getUniformSelection(k);
                     BiddingStrategy[] bs3 = BiddingStrategy.compareTwoStrategies(BiddingStrategy.Version1,BiddingStrategy.Version2,k);
 
-                    Simulation s = new Simulation(bs,n,r,sMax,e,pure);
+                    Simulation s = new Simulation(bs1,n,r,sMax,e,pure,true);
                     s.simulate();
                     s.printout();
                 }
@@ -61,36 +61,74 @@ public class OnceBrassCollusion {
     private class Simulation {
         private final double sMax;
         private final BiddingStrategy[] bss;
+        private final double[][] biddingFactor;
         private final int r;
         private final int n;
         private final boolean pure;
         private final double e; //punishment factor
+        private final double[] totalProfitBuyer;
+        private final double[] totalProfitSeller;
+        private final boolean print;
         double[][] profitSellerInRoundR;
         double[][] profitBuyerInRoundR;
+        private final double[][] marketPriceInRoundR;
 
-        public Simulation(BiddingStrategy[] bs, int n, int r, double sMax, double e, boolean pure) {
+        public Simulation(BiddingStrategy[] bs, int n, int r, double sMax, double e, boolean pure,boolean print) {
             this.sMax = sMax;
             this.bss = bs;
             this.n = n;
+            this.print = print;
             profitSellerInRoundR = new double[r][n];
             profitBuyerInRoundR = new double[r][bs.length];
+            totalProfitSeller = new double[n];
+            totalProfitBuyer = new double[bs.length];
+            biddingFactor = createBiddingFactor(bs.length,n); //for every buyer seller combination a bidding factor
+
+            marketPriceInRoundR = new double[r][];
+
             this.r = r;
             this.pure = pure;
             this.e = e;
 
+
+        }
+
+        private double[][] createBiddingFactor(int k, int n) {
+            double[][] biddinFactor = new double[k][n];
+            for (int i = 0; i < k; i++) {
+                for (int j = 0; j < n; j++) {
+                    biddinFactor[i][j] = rng.nextDouble()*4+1; //it doesnt make sense to bid less then the starting price
+                }
+            }
+            return biddinFactor;
         }
 
         public void simulate() {
             for (int r_i = 0; r_i < r; r_i++) { //for each round
-
+                if (print) {
+                    System.out.println("This shall be round " + r_i);
+                    System.out.println();
+                }
                 double[] stuffToBuyStartingPrice = createStartingPrices(n,sMax);
-                double[][] bidsPerItem = recieveBidsPerItem(stuffToBuyStartingPrice,bss,r_i);
+                double[][] bidsPerItem = recieveBidsPerItem(stuffToBuyStartingPrice,bss,biddingFactor,r_i);
                 double[] markedPrices = calcMarketPrices(bidsPerItem);
-
+                marketPriceInRoundR[r_i] = markedPrices;
                 double[] profitSellerInCurrentRound = profitSellerInRoundR[r_i];
                 double[] profitBuyerInCurrentRound = profitBuyerInRoundR[r_i];
-                determineWinnerAndCalculateProfit(markedPrices,bidsPerItem,profitBuyerInCurrentRound,profitSellerInCurrentRound,pure,e);
+                determineWinnerAndCalculateProfit(stuffToBuyStartingPrice,markedPrices,bidsPerItem,profitBuyerInCurrentRound,profitSellerInCurrentRound,pure,e);
             }
+            //calc total profit
+            for (int n_i = 0; n_i < n; n_i++) {
+                for (int r_i = 0; r_i < r; r_i++) {
+                   totalProfitSeller[n_i] += profitSellerInRoundR[r_i][n_i];
+                }
+            }
+            for (int k_i = 0; k_i < bss.length; k_i++) {
+                for (int r_i = 0; r_i < r; r_i++) {
+                    totalProfitBuyer[k_i] += profitBuyerInRoundR[r_i][k_i];
+                }
+            }
+
 
         }
 
@@ -115,7 +153,7 @@ public class OnceBrassCollusion {
             return order;
         }
 
-        private void determineWinnerAndCalculateProfit(double[] markedPrices, double[][] bidsPerItem, double[] profitBuyerInCurrentRound, double[] profitSellerInCurrentRound, boolean pure, double e) {
+        private void determineWinnerAndCalculateProfit(double[] stuffToBuyStartingPrice, double[] markedPrices, double[][] bidsPerItem, double[] profitBuyerInCurrentRound, double[] profitSellerInCurrentRound, boolean pure, double e) {
             int numberBuyers = bidsPerItem[0].length;
             boolean[] hasPlayerWonInThisRound = new boolean[numberBuyers];
             int[] fromWichSellerDidBuyerBuy = new int[numberBuyers];
@@ -126,6 +164,10 @@ public class OnceBrassCollusion {
             //determine winner for one round
             while(it.hasNext()){
                 int i = it.next();
+                if (print) {
+                    System.out.println("Item item item from Seller " + i + " Starting price: " + stuffToBuyStartingPrice[i]);
+                    System.out.println();
+                }
                 double[] bidsOfParticularItem = bidsPerItem[i];
                 double marketPriceOfParticularItem = markedPrices[i];
 
@@ -134,33 +176,51 @@ public class OnceBrassCollusion {
 
                 //iterate through the ranking until winner is reached
                 int indexOfWinnerInRanking = 0;
-                while (bidsOfParticularItem[ranking[indexOfWinnerInRanking]] > marketPriceOfParticularItem ||
-                    !winnerWantsItem(pure,hasPlayerWonInThisRound[indexOfWinnerInRanking],profitBuyerInCurrentRound[indexOfWinnerInRanking],bidsOfParticularItem[ranking[indexOfWinnerInRanking+1]],e))
+                int n_i = ranking[indexOfWinnerInRanking];
+                double payPrice = stuffToBuyStartingPrice[i];
+                while (bidsOfParticularItem[n_i] > marketPriceOfParticularItem ||
+                    !winnerWantsItem(pure,hasPlayerWonInThisRound[n_i],profitBuyerInCurrentRound[n_i],bidsOfParticularItem[ranking[indexOfWinnerInRanking+1]],e)){
                     indexOfWinnerInRanking++;
+                    if(indexOfWinnerInRanking==ranking.length-1){
+                        //apperently all prices are the same or sombody doesnt want it, lets give it to the second to last
+                        indexOfWinnerInRanking=ranking.length-2;
+                        payPrice = bidsOfParticularItem[ranking[indexOfWinnerInRanking+1]];
+                        break;
+                    }
+
+
+                    n_i = ranking[indexOfWinnerInRanking];
+                    payPrice = bidsOfParticularItem[ranking[indexOfWinnerInRanking+1]];
+
+                }
+
                 //we have a winner
-                double payPrice = bidsOfParticularItem[ranking[indexOfWinnerInRanking+1]];
+                assert (payPrice>0);
                 assert (profitSellerInCurrentRound[i]<0.000001);
                 if (pure){
                     profitSellerInCurrentRound[i]=payPrice;
                     double profitBuyer = marketPriceOfParticularItem-payPrice;
-                    assert (profitBuyerInCurrentRound[indexOfWinnerInRanking]<0.0000001);
-                    profitBuyerInCurrentRound[indexOfWinnerInRanking]=profitBuyer;
+                    assert (profitBuyerInCurrentRound[n_i]<0.0000001);
+                    profitBuyerInCurrentRound[n_i]=profitBuyer;
 
                 }else {
-                    if (hasPlayerWonInThisRound[indexOfWinnerInRanking]){
+                    if (hasPlayerWonInThisRound[n_i]){
 
-                        double punish = profitBuyerInCurrentRound[indexOfWinnerInRanking]*e;
-                        profitBuyerInCurrentRound[indexOfWinnerInRanking] = payPrice-punish;
-                        profitSellerInCurrentRound[indexOfWinnerInRanking] = payPrice;
-                        profitSellerInCurrentRound[fromWichSellerDidBuyerBuy[indexOfWinnerInRanking]] = punish;
+                        double punish = profitBuyerInCurrentRound[n_i]*e;
+
+                        assert (punish>0);
+                        profitBuyerInCurrentRound[n_i] = payPrice-punish;
+                        profitSellerInCurrentRound[i] = payPrice;
+                        profitSellerInCurrentRound[fromWichSellerDidBuyerBuy[n_i]] = punish;
                     }else{
-                        profitBuyerInCurrentRound[indexOfWinnerInRanking] = payPrice;
-                        profitSellerInCurrentRound[indexOfWinnerInRanking] = payPrice;
+                        profitBuyerInCurrentRound[n_i] = payPrice;
+                        profitSellerInCurrentRound[i] = payPrice;
                     }
-                    fromWichSellerDidBuyerBuy[indexOfWinnerInRanking] = i;
+                    fromWichSellerDidBuyerBuy[n_i] = i;
                 }
 
-                hasPlayerWonInThisRound[indexOfWinnerInRanking] = true;
+                hasPlayerWonInThisRound[n_i] = true;
+
             }
 
 
@@ -200,10 +260,11 @@ public class OnceBrassCollusion {
          *
          * @param stuffToBuyStartingPrice
          * @param bss the bidding strategies
+         * @param biddingFactor
          * @param r curent round
          * @return an array which contains for every item a bid of every buyer
          */
-        private double[][] recieveBidsPerItem(double[] stuffToBuyStartingPrice, BiddingStrategy[] bss,int r) {
+        private double[][] recieveBidsPerItem(double[] stuffToBuyStartingPrice, BiddingStrategy[] bss, double[][] biddingFactor, int r) {
             int k = stuffToBuyStartingPrice.length;
             int n = bss.length;
 
@@ -212,7 +273,11 @@ public class OnceBrassCollusion {
                 double startingPrice = stuffToBuyStartingPrice[k_i];
                 double[] bidsForParticularItem = bidsPerItem[k_i];
                 for (int n_i = 0; n_i < n; n_i++) {
-                    bidsForParticularItem[n_i] = bss[n_i].bid(profitSellerInRoundR,profitBuyerInRoundR,r,startingPrice);
+
+                    if (k_i == 6){
+                        System.out.println(k_i + " " + n_i);
+                    }
+                    bidsForParticularItem[n_i] = bss[n_i].bid(profitSellerInRoundR,profitBuyerInRoundR,r,startingPrice,biddingFactor[n_i][k_i]);
                 }
 
             }
@@ -220,28 +285,52 @@ public class OnceBrassCollusion {
         }
 
         private double[] createStartingPrices(int n, double sMax) {
-            return new double[0];
+            //its been said, that sellers want to maximise their profits, but i dont see it mentioned further
+            double[] prizeOfEachSeller = new double[n];
+            for (int i = 0; i < n; i++) {
+                prizeOfEachSeller[i] = rng.nextDouble()*sMax;
+            }
+            return prizeOfEachSeller;
         }
 
         public void printout() {
+            for (int n_i = 0; n_i < marketPriceInRoundR[0].length; n_i++) {
+                System.out.println("Market Prices for seller " + n_i);
+                for (int r_i = 0; r_i < r; r_i++) {
+                    System.out.print(marketPriceInRoundR[r_i][n_i] + ",");
+                }
+                System.out.println();
+                System.out.println();
+            }
 
+            System.out.println("Profit for seller:");
+            for (int n_i = 0; n_i < totalProfitSeller.length; n_i++) {
+                    System.out.print(totalProfitSeller[n_i] + ",");
+            }
+            System.out.println();
+            System.out.println("Profit for buyer:");
+            for (int k_i = 0; k_i < totalProfitBuyer.length; k_i++) {
+                System.out.print(totalProfitBuyer[k_i] + ",");
+            }
+            System.out.println();
+            System.out.println();
         }
     }
 
     private enum BiddingStrategy {
         Version1{
             @Override
-            public double bid(double[][] profitSellerInRoundR, double[][] profitBuyerInRoundR, int r, double startingPrice) {
-                return 0;
+            public double bid(double[][] profitSellerInRoundR, double[][] profitBuyerInRoundR, int r, double startingPrice, double biddingFactor) {
+                return startingPrice*biddingFactor;
             }
         },Version2{
             @Override
-            public double bid(double[][] profitSellerInRoundR, double[][] profitBuyerInRoundR, int r, double startingPrice) {
+            public double bid(double[][] profitSellerInRoundR, double[][] profitBuyerInRoundR, int r, double startingPrice, double biddingFactor) {
                 return 0;
             }
         },Version3{
             @Override
-            public double bid(double[][] profitSellerInRoundR, double[][] profitBuyerInRoundR, int r, double startingPrice) {
+            public double bid(double[][] profitSellerInRoundR, double[][] profitBuyerInRoundR, int r, double startingPrice, double biddingFactor) {
                 return 0;
             }
         };
@@ -270,7 +359,7 @@ public class OnceBrassCollusion {
             return bs;
         }
 
-        public abstract double bid(double[][] profitSellerInRoundR, double[][] profitBuyerInRoundR, int r, double startingPrice);
+        public abstract double bid(double[][] profitSellerInRoundR, double[][] profitBuyerInRoundR, int r, double startingPrice, double biddingFactor);
     }
 
 
